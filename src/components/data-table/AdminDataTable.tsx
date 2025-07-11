9caf968bfd4eb16ecb7768fc369753cefa5da541
PorKeat <alexkgm2412@gmail.com>
2025-07-11 08:39:07 +0700

    modified:react-table

--- src/components/data-table/AdminDataTable.tsx
+++ src/components/data-table/AdminDataTable.tsx
@@ -1,47 +1,78 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import { UserType } from "@/types/userType";
+import { useEffect, useState } from "react";
 import DataTable, { TableColumn } from "react-data-table-component";
 
-type Movie = {
-  id: number;
-  title: string;
-  year: string;
-};
-
-const columns:TableColumn<Movie>[] = [
-  {
-    name: "Title",
-    selector: (row) => row.title,
-  },
-  {
-    name: "Year",
-    selector: (row) => row.year,
-  },
-];
-const data: Movie[] = [
-  {
-    id: 1,
-    title: "Beetlejuice",
-    year: "1988",
-  },  
-  {
-    id: 2,
-    title: "Ghostbusters",
-    year: "1984",
-  },
-];
-
 export function AdminDataTable() {
-  // const [isClient, setIsClient] = useState(false);
+  const [users, setUser] = useState<UserType[]>([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_BASE_URL_API}users`
+        );
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        console.log(data.users);
+        setUser(data.users);
+      } catch (error) {
+        console.error("Error fetching users:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchUsers();
+  }, []);
 
-  // useEffect(() => {
-  //   setIsClient(true);
-  // }, []);
+  const columns: TableColumn<UserType>[] = [
+    {
+      name: "ID",
+      selector: (row: UserType) => row.id,
+      width: "100px",
+    },
+    {
+      name: "Username",
+      selector: (row: UserType) => row.firstName + " " + row.lastName,
+      width: "250px",
+    },
+    {
+      name: "Age",
+      selector: (row: UserType) => row.age,
+      width: "170px",
+    },
+    {
+      name: "Gender",
+      selector: (row: UserType) => row.gender,
+      width: "170px",
+    },
+    {
+      name: "Gender",
+      selector: (row: UserType) => row.birthDate,
+      width: "170px",
+    },
+    {
+      name: "Country",
+      selector: (row: UserType) => row.address.country,
+      width: "150px",
+    },
+    {
+      name: "Email",
+      selector: (row: UserType) => row.email,
+      width: "280px",
+    },
+  ];
 
-  // if (!isClient) {
-  //   return <h1>Loading...</h1>;
-  // }
-
-  return <DataTable columns={columns} data={data}  />;
+  return (
+    <DataTable
+      title="User List"
+      columns={columns}
+      data={users}
+      progressPending={loading}
+      pagination
+    />
+  );
 }
--- src/types/userType.ts
+++ src/types/userType.ts
@@ -1,26 +1,31 @@
 export type UserType = {
-    id: number;
-    firstName: string;
-    lastName: string;
-    username: string;
-    email: string;
-    image: string;
-}
-
-export type UserDetailType = {
-    id: number;
-    firstName: string;
-    lastName: string;
-    username: string;
-    email: string;
-    image: string;
-    address: {
-        street: string;
-        city: string;
-        state: string;
-        zipCode: string;
-    };
-    phoneNumber: string;
-    website: string;
+  id: number;
+  firstName: string;
+  lastName: string;
+  username: string;
+  age: string;
+  gender: string;
+  birthDate: string;
+  address: {
+    country: string;
+  };
+  email: string;
+  image: string;
 };
 
+export type UserDetailType = {
+  id: number;
+  firstName: string;
+  lastName: string;
+  username: string;
+  email: string;
+  image: string;
+  address: {
+    street: string;
+    city: string;
+    state: string;
+    zipCode: string;
+  };
+  phoneNumber: string;
+  website: string;
+};
